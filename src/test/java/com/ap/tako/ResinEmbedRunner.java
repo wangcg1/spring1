963e1325d99d961aba638ad4798b3059683ded20
wangcg <galenwangster@gmail.com>
2014-09-12 11:02:45 +0800

    addnew
    addnew

---
+++ src/test/java/com/ap/tako/ResinEmbedRunner.java
@@ -0,0 +1,31 @@
+package test.java.com.ap.tako;
+
+
+//import com.caucho.resin.HttpEmbed;
+//import com.caucho.resin.ResinEmbed;
+//import com.caucho.resin.WebAppEmbed;
+
+public class ResinEmbedRunner {
+
+	
+	public static void main(String[] args) throws Exception {
+
+//		//configuration settings
+//		int webAppPort = 80;
+//		
+//		//相对地址：WebRoot
+//		String webAppRootPath = "src/main/webapp";
+//
+//		//ResinEmbed web server for development phase
+//		final ResinEmbed resin = new ResinEmbed();
+//		HttpEmbed http = new HttpEmbed(webAppPort);
+//		resin.addPort(http);
+//		WebAppEmbed webApp = new WebAppEmbed("/", webAppRootPath);
+//		resin.addWebApp(webApp);
+//
+//		resin.start();
+//		resin.join();
+		
+	}
+
+}
